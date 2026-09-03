497157f3621abbc3fb5a6c2d1e128927e64a4c7b
shuvotrading35-a11y <shuvotrading35@gmail.com>
2026-09-03 12:46:25 +0600

    Update next.config.js

--- merchant-panel/next.config.js
+++ merchant-panel/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+  },
 };
 
 module.exports = nextConfig;
